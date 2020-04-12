20260c7ba3bad7cd09f6be2d4ffb094bd820c32c
tanvir577 <46286999+tanvir577@users.noreply.github.com>
2020-04-12 20:14:47 +0600

    zero

---
+++ zero/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float moveSpeed;
+    public float gravity = -9.8f;
+    Vector3 velocity;
+ 
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = Vector3.right * x + Vector3.forward * z;
+
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
---
+++ zero/Assets/Scripts/player_look.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_look : MonoBehaviour
+{
+    public Transform playerBody;
+    public float mouseSecsivity = 100f;
+    float xAxisRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSecsivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSecsivity * Time.deltaTime;
+
+        xAxisRotation -= mouseY;
+        xAxisRotation = Mathf.Clamp(xAxisRotation, -90f, 90f);
+
+        transform.localRotation=Quaternion.Euler(xAxisRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
